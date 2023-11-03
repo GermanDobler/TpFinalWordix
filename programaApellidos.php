df47fb1b181a606f9b9d8d97043c656c62f266e2
German <german.crack@hotmail.com>
2023-11-02 23:58:05 -0300

    Feat: Lista de partidas

--- programaApellidos.php
+++ programaApellidos.php
@@ -26,16 +26,54 @@ function cargarColeccionPalabras()
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
         "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
         "LETRA", "MARZO", "LUNES", "MANGO", "NADAR"
-        
-        
-        /* Agregar 5 palabras más: YA REALIZADO*/ 
+
+
+        /* Agregar 5 palabras más: YA REALIZADO*/
     ];
 
     return ($coleccionPalabras);
 }
 
-/* ****COMPLETAR***** */
+/**
+ * Obtiene una colección de partidas
+ * @return string
+ */
 
+function mostrarListadoPartidas()
+{
+    $partidas = [
+        "1" => [
+            "usuario" => "MATI TORRE GATO",
+            "palabra" => "MELON",
+            "intentos" => 3,
+            "estado" => "Perdida",
+            "puntaje" => 100,
+        ],
+        "2" => [
+            "usuario" => "MARTIN DEMICHELI",
+            "palabra" => "MELON",
+            "intentos" => 3,
+            "estado" => "Ganada",
+            "puntaje" => 10000,
+        ],
+        "3" => [
+            "usuario" => "EL GERMAN",
+            "palabra" => "MELON",
+            "intentos" => 3,
+            "estado" => "Ganada",
+            "puntaje" => 10000,
+        ],
+    ];
+    foreach ($partidas as $partida => $value) {
+        echo "Partida: $partida\n";
+        echo "Usuario: $value[usuario]\n";
+        echo "Palabra: $value[palabra]\n";
+        echo "Intentos: $value[intentos]\n";
+        echo "Estado: $value[estado]\n";
+        echo "Puntaje: $value[puntaje]\n";
+        echo "---------------------------------\n";
+    }
+}
 
 
 /**************************************/
@@ -50,56 +88,62 @@ function cargarColeccionPalabras()
 
 //Proceso:
 
-$partida = jugarWordix("MELON", strtolower("MaJo"));
+// $partida = jugarWordix("MELON", strtolower("MaJo"));
 //print_r($partida);
 //imprimirResultado($partida);
 
 
 
 do {
-    $opcion=seleccionarOpcion();
-    $cantPalabras=count(cargarColeccionPalabras());
-    $coleccionPalabras=cargarColeccionPalabras();
+    $opcion = seleccionarOpcion();
+    $cantPalabras = count(cargarColeccionPalabras());
+    $coleccionPalabras = cargarColeccionPalabras();
 
 
     switch ($opcion) {
-        case 1: 
+        case 1:
             //Jugar al wordix con una palabra elegida
             echo "Ingrese su usuario: ";
-            $usuario=trim(fgets(STDIN));
+            $usuario = trim(fgets(STDIN));
             echo "Ingrese el numero de palabra para jugar(entre 1 y $cantPalabras ): ";
-            $eleccion=trim(fgets(STDIN))-1;
-            if(($eleccion>=0)&&($eleccion<$cantPalabras)){
-                $palabraElegida= $coleccionPalabras[$eleccion];
+            $eleccion = trim(fgets(STDIN)) - 1;
+            if (($eleccion >= 0) && ($eleccion < $cantPalabras)) {
+                $palabraElegida = $coleccionPalabras[$eleccion];
                 jugarWordix($palabraElegida, strtolower($usuario));
-            }else{
+            } else {
                 echo "ERROR MOGOLICO\n";
             }
-            
+
             break;
-        case 2: 
-           // Jugar al wordix con una palabra aleatoria
+        case 2:
+            // Jugar al wordix con una palabra aleatoria
 
 
             break;
-        case 3: 
+        case 3:
             //Mostrar una partida
 
             break;
         case 4:
             //Mostrar la primer partida ganadora
             //...
+            break;
         case 5:
             //Mostrar resumen de Jugador
-
+            break;
         case 6:
-             //Mostrar listado de partidas ordenadas por jugador y por palabra
-
+            //Mostrar listado de partidas ordenadas por jugador y por palabra
+            mostrarListadoPartidas();
+            break;
         case 7:
             //Agregar una palabra de 5 letras a Wordix
-        
+            break;
         case 8:
-            //Salir
-
+            //borrar consola 
+            // system('cls');
+            echo "Gracias por jugar a Wordix\n";
+            break;
+        default:
+            echo "Opción inválida\n";
     }
 } while ($opcion != 8);
